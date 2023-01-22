e01e24c38c6fa4ffb54b1259c2f507946762704b
sebastiensimoen <123328668+sebastiensimoen@users.noreply.github.com>
2023-01-22 21:17:14 +0100

    Create CG_Easy_01_The_Descent.py
    https://www.codingame.com/training/easy/the-descent

---
+++ Python/CG_Easy_01_The_Descent.py
@@ -0,0 +1,22 @@
+import sys
+import math
+
+# The while loop represents the game.
+# Each iteration represents a turn of the game
+# where you are given inputs (the heights of the mountains)
+# and where you have to print an output (the index of the mountain to fire on)
+# The inputs you are given are automatically updated according to your last actions.
+
+# game loop
+while True:
+    max = 0
+    index = -1
+
+    for i in range(8):
+        mountain_h = int(input())  # represents the height of one mountain.
+        if mountain_h > max:
+            max = mountain_h
+            index = i
+    # Write an action using print
+    # The index of the mountain to fire on.
+    print(index)
